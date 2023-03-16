c02c2c07b14fbac68b46af1fefc5d1b2ed7445d9
Catherine <catherine.wambui@student.moringaschool.com>
2023-03-16 23:57:48 +0300

    lab completed

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function(num1=2000,num2=100){
+    return num1/num2
+}
+
+console.log(divide());
+
+const square = x => x * x
+
+console.log(square(5));
+
+const add = (numa,numb) => numa + numb
+
+console.log(add(67,90))
